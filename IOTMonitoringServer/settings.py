fd2210db5d1e6ab0ef190790c6e43929d5c60c23
hvaron83 <h.varon@uniandes.edu.co>
2022-07-16 23:38:58 -0500

    Corrección IP DB

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "33.81.37.205",  # Dirección IP de la base de datos
+        "HOST": "3.81.37.205",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
