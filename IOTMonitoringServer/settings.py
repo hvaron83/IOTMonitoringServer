4c02fefca001070e9cfdf20ee9f7ada87f6ddfd4
hvaron83 <h.varon@uniandes.edu.co>
2022-07-23 12:40:17 -0500

    Actualizacion IP servicios

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "52.200.172.180"]
+ALLOWED_HOSTS = ["localhost", "3.89.206.236"]
 
 
 # Application definition
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "3.81.68.79",  # Dirección IP de la base de datos
+        "HOST": "54.91.155.188",  # Dirección IP de la base de datos
         "PORT": "5432",  # Puerto de la base de datos
     }
 }
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "18.232.129.138"
+MQTT_HOST = "50.19.145.43"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
